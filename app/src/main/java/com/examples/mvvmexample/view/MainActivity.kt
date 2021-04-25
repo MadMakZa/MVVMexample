3a9cce761dcd9dacbfa5be0a69503f5e94a8aeaa
MadMakZa <immakza@gmail.com>
2021-04-25 23:19:11 +0300

    add binding viewModel

---
+++ app/src/main/java/com/examples/mvvmexample/view/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.examples.mvvmexample.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.examples.mvvmexample.R
+import com.examples.mvvmexample.databinding.ActivityMainBinding
+import com.examples.mvvmexample.viewModel.MainViewModel
+
+/**
+ * View - представление
+ */
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    var mainViewModel = MainViewModel() //View должна иметь экземпляр ViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = mainViewModel
+        binding.executePendingBindings()
+    }
+}
--- app/src/main/java/com/examples/mvvmexample/viewModel/MainViewModel.kt
+++ app/src/main/java/com/examples/mvvmexample/viewModel/MainViewModel.kt
@@ -1,12 +1,34 @@
 package com.examples.mvvmexample.viewModel
 
+import androidx.databinding.ObservableField
+import com.examples.mvvmexample.model.OnDataReadyCallback
 import com.examples.mvvmexample.model.RepoModel
+import java.util.*
 
 /**
  * ViewModel - logic
  */
 class MainViewModel {
-    var repoModel: RepoModel = RepoModel() //экземпляр класса model
-    var text: String = ""
-    var isLoading: Boolean = false
+    var repoModel: RepoModel = RepoModel()  //экземпляр класса model
+    val text = ObservableField<String>()    //оборачиваем объекты для наблюдения за ними (dataBinding)
+    val isLoading = ObservableField<Boolean>()
+
+    //метод отвечающий за обновление данных
+    val onDataReadyCallback = object : OnDataReadyCallback{
+        override fun onDataReady(data: String) {
+            isLoading.set(false)
+            text.set(data)
+        }
+    }
+
+    //вызывает refreshData у RepoModel, который в аргументах берёт реализацию OnDataReadyCallback
+    fun refresh(){
+        isLoading.set(true)
+        repoModel.refreshData(object : OnDataReadyCallback {
+            override fun onDataReady(data: String) {
+                isLoading.set(false)
+                text.set(data)
+            }
+        })
+    }
 }
